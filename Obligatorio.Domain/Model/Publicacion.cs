cc9ddd8814085dc8991b68eac09da60fd83289cf
icalderazzo <ignaciocalderazzo@gmail.com>
2021-10-09 21:05:24 -0300

    Modelado version 1

---
+++ Obligatorio.Domain/Model/Publicacion.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+namespace Obligatorio.Domain.Model
+{
+    public class Publicacion
+    {
+        public Producto Producto { get; set; }
+        public int CiUsuario { get; set; }
+        public bool Estado { get; set; }
+        public DateTime FechaPublicacion { get; set; }
+    }
+}

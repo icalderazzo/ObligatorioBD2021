ed4559f0bfd408b8c3cff00ad0ae03cad2bd79e8
icalderazzo <ignaciocalderazzo@gmail.com>
2021-10-27 01:23:27 -0300

    Listado publicaciones
    * Se agrega post repo y post service
    * Se obtiene el listado en home form
    * Se utiliza Dependency injection en los forms

--- Presentation/Forms/HomeForm.cs
+++ Presentation/Forms/HomeForm.cs
@@ -1,17 +1,39 @@
-﻿using System.Windows.Forms;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Obligatorio.Domain.Model;
+using Obligatorio.Services.Interfaces;
 
 namespace Presentation.Forms
 {
     public partial class HomeForm : Form
     {
-        public HomeForm()
+        private readonly IPostsService _postsService;
+
+        public HomeForm(IPostsService postsService)
         {
+            _postsService = postsService;
             InitializeComponent();
         }
 
-        private void HomeForm_Load(object sender, System.EventArgs e)
+        private async void HomeForm_Load(object sender, System.EventArgs e)
         {
+            await LoadFeed();
+        }
 
+        private async Task LoadFeed()
+        {
+            var posts = await GetActivePosts();
+            foreach (var post in posts)
+            {
+
+            }
+        }
+
+        private async Task<List<Publicacion>> GetActivePosts()
+        {
+            var posts = await Task.Run(() => _postsService.ListForFeed(Global.LoggedUser.Cedula));
+            return posts;
         }
     }
 }

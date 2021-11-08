ae074c67f8420d1341540921a3cdf756d2119e2e
icalderazzo <ignaciocalderazzo@gmail.com>
2021-11-08 13:53:28 -0300

    add comment to FilterOffers

--- Obligatorio.Services/Interfaces/IOfferService.cs
+++ Obligatorio.Services/Interfaces/IOfferService.cs
@@ -6,6 +6,11 @@ namespace Obligatorio.Services.Interfaces
 {
     public interface IOfferService : IService<Oferta>
     {
+        /// <summary>
+        /// Filtrar ofertas, con un estado determinado, en las que participa un usuario con un rol específico.
+        /// </summary>
+        /// <param name="filter">Filtro con: ciUsuario, RolOferta, EstadoOferta</param>
+        /// <returns>Listado de ofertas filtradas</returns>
         List<Oferta> FilterOffers(OfferFilter filter);
         public void AcceptOffer(Oferta offer);
         public void CancelOffer(Oferta offer);

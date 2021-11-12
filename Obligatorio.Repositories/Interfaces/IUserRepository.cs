502c0dd2d3875aff06253e5d06f3f0ffc52c2f0f
icalderazzo <ignaciocalderazzo@gmail.com>
2021-11-12 20:06:10 -0300

    Add checks with <> Ci

--- Obligatorio.Repositories/Interfaces/IUserRepository.cs
+++ Obligatorio.Repositories/Interfaces/IUserRepository.cs
@@ -5,9 +5,9 @@ namespace Obligatorio.Repositories.Interfaces
     public interface IUserRepository : IRepository<Usuario>
     {
         Usuario GetForLogin(string username, string password);
-        bool ExistsUserWithUsername(string username);
-        bool ExistsUserWithPhoneNumber(int phoneNumber);
+        bool ExistsUserWithUsername(string username, int ci);
+        bool ExistsUserWithPhoneNumber(int phoneNumber, int ci);
         bool ExistsUserWithCi(int ci);
-        bool ExistsUserWithEmail(string email);
+        bool ExistsUserWithEmail(string email, int ci);
     }
 }

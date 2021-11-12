24e8b43e741456598dab3bdb3a8f8ef480e83b63
icalderazzo <ignaciocalderazzo@gmail.com>
2021-11-12 14:45:07 -0300

    Update user form

--- Presentation/Forms/EditUserFrom.Designer.cs
+++ Presentation/Forms/EditUserFrom.Designer.cs
@@ -28,12 +28,299 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtPhoneNumber = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.txtCurrentPasswd = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblCurrentPasswd = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnChangePassword = new System.Windows.Forms.Button();
+            this.lblNewPasswd = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.lblCi = new System.Windows.Forms.Label();
+            this.lblRepeatNewPasswd = new System.Windows.Forms.Label();
+            this.txtNewPasswd = new System.Windows.Forms.TextBox();
+            this.txtRepeatNewPasswd = new System.Windows.Forms.TextBox();
+            this.btnUpdateUser = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtPhoneNumber
+            // 
+            this.txtPhoneNumber.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtPhoneNumber.Location = new System.Drawing.Point(221, 294);
+            this.txtPhoneNumber.MaxLength = 9;
+            this.txtPhoneNumber.Name = "txtPhoneNumber";
+            this.txtPhoneNumber.Size = new System.Drawing.Size(199, 34);
+            this.txtPhoneNumber.TabIndex = 27;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtEmail.Location = new System.Drawing.Point(220, 367);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(199, 34);
+            this.txtEmail.TabIndex = 26;
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtSurname.Location = new System.Drawing.Point(221, 217);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(199, 34);
+            this.txtSurname.TabIndex = 25;
+            // 
+            // txtCurrentPasswd
+            // 
+            this.txtCurrentPasswd.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtCurrentPasswd.Location = new System.Drawing.Point(891, 145);
+            this.txtCurrentPasswd.Name = "txtCurrentPasswd";
+            this.txtCurrentPasswd.Size = new System.Drawing.Size(199, 34);
+            this.txtCurrentPasswd.TabIndex = 24;
+            this.txtCurrentPasswd.Visible = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(86, 375);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(77, 23);
+            this.label7.TabIndex = 23;
+            this.label7.Text = "Correo";
+            // 
+            // lblCurrentPasswd
+            // 
+            this.lblCurrentPasswd.AutoSize = true;
+            this.lblCurrentPasswd.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblCurrentPasswd.ForeColor = System.Drawing.Color.White;
+            this.lblCurrentPasswd.Location = new System.Drawing.Point(612, 152);
+            this.lblCurrentPasswd.Name = "lblCurrentPasswd";
+            this.lblCurrentPasswd.Size = new System.Drawing.Size(196, 23);
+            this.lblCurrentPasswd.TabIndex = 22;
+            this.lblCurrentPasswd.Text = "Contraseña actual";
+            this.lblCurrentPasswd.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(85, 301);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 23);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Teléfono";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(86, 222);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 23);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Apellido";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(85, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 23);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Nombre";
+            // 
+            // btnChangePassword
+            // 
+            this.btnChangePassword.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnChangePassword.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnChangePassword.Location = new System.Drawing.Point(959, 12);
+            this.btnChangePassword.Name = "btnChangePassword";
+            this.btnChangePassword.Size = new System.Drawing.Size(178, 29);
+            this.btnChangePassword.TabIndex = 28;
+            this.btnChangePassword.Text = "Cambiar contraseña";
+            this.btnChangePassword.UseVisualStyleBackColor = true;
+            this.btnChangePassword.Click += new System.EventHandler(this.btnChangePassword_Click);
+            // 
+            // lblNewPasswd
+            // 
+            this.lblNewPasswd.AutoSize = true;
+            this.lblNewPasswd.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblNewPasswd.ForeColor = System.Drawing.Color.White;
+            this.lblNewPasswd.Location = new System.Drawing.Point(612, 234);
+            this.lblNewPasswd.Name = "lblNewPasswd";
+            this.lblNewPasswd.Size = new System.Drawing.Size(195, 23);
+            this.lblNewPasswd.TabIndex = 29;
+            this.lblNewPasswd.Text = "Nueva contraseña";
+            this.lblNewPasswd.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(521, 327);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 23);
+            this.label5.TabIndex = 30;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtName);
+            this.groupBox1.Controls.Add(this.lblUsername);
+            this.groupBox1.Controls.Add(this.lblCi);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtPhoneNumber);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtEmail);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.txtSurname);
+            this.groupBox1.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(534, 455);
+            this.groupBox1.TabIndex = 31;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Datos personales";
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtName.Location = new System.Drawing.Point(221, 133);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(199, 34);
+            this.txtName.TabIndex = 30;
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblUsername.ForeColor = System.Drawing.Color.White;
+            this.lblUsername.Location = new System.Drawing.Point(23, 78);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(287, 21);
+            this.lblUsername.TabIndex = 29;
+            this.lblUsername.Text = "Nombre de usuario: [Username]";
+            // 
+            // lblCi
+            // 
+            this.lblCi.AutoSize = true;
+            this.lblCi.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblCi.ForeColor = System.Drawing.Color.White;
+            this.lblCi.Location = new System.Drawing.Point(23, 42);
+            this.lblCi.Name = "lblCi";
+            this.lblCi.Size = new System.Drawing.Size(116, 21);
+            this.lblCi.TabIndex = 28;
+            this.lblCi.Text = "Cédula: [Ci]";
+            // 
+            // lblRepeatNewPasswd
+            // 
+            this.lblRepeatNewPasswd.AutoSize = true;
+            this.lblRepeatNewPasswd.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblRepeatNewPasswd.ForeColor = System.Drawing.Color.White;
+            this.lblRepeatNewPasswd.Location = new System.Drawing.Point(612, 296);
+            this.lblRepeatNewPasswd.Name = "lblRepeatNewPasswd";
+            this.lblRepeatNewPasswd.Size = new System.Drawing.Size(266, 23);
+            this.lblRepeatNewPasswd.TabIndex = 32;
+            this.lblRepeatNewPasswd.Text = "Repetir nueva contraseña";
+            this.lblRepeatNewPasswd.Visible = false;
+            // 
+            // txtNewPasswd
+            // 
+            this.txtNewPasswd.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtNewPasswd.Location = new System.Drawing.Point(891, 229);
+            this.txtNewPasswd.Name = "txtNewPasswd";
+            this.txtNewPasswd.Size = new System.Drawing.Size(199, 34);
+            this.txtNewPasswd.TabIndex = 31;
+            this.txtNewPasswd.Visible = false;
+            // 
+            // txtRepeatNewPasswd
+            // 
+            this.txtRepeatNewPasswd.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtRepeatNewPasswd.Location = new System.Drawing.Point(891, 289);
+            this.txtRepeatNewPasswd.Name = "txtRepeatNewPasswd";
+            this.txtRepeatNewPasswd.Size = new System.Drawing.Size(199, 34);
+            this.txtRepeatNewPasswd.TabIndex = 33;
+            this.txtRepeatNewPasswd.Visible = false;
+            // 
+            // btnUpdateUser
+            // 
+            this.btnUpdateUser.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnUpdateUser.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(249)))), ((int)(((byte)(237)))));
+            this.btnUpdateUser.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnUpdateUser.FlatAppearance.BorderSize = 0;
+            this.btnUpdateUser.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUpdateUser.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnUpdateUser.Location = new System.Drawing.Point(1011, 433);
+            this.btnUpdateUser.Name = "btnUpdateUser";
+            this.btnUpdateUser.Size = new System.Drawing.Size(126, 33);
+            this.btnUpdateUser.TabIndex = 34;
+            this.btnUpdateUser.Text = "Actualizar";
+            this.btnUpdateUser.UseVisualStyleBackColor = false;
+            this.btnUpdateUser.Click += new System.EventHandler(this.btnUpdateUser_Click);
+            // 
+            // EditUserFrom
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(68)))), ((int)(((byte)(107)))));
+            this.ClientSize = new System.Drawing.Size(1149, 479);
+            this.Controls.Add(this.btnUpdateUser);
+            this.Controls.Add(this.txtRepeatNewPasswd);
+            this.Controls.Add(this.txtNewPasswd);
+            this.Controls.Add(this.lblRepeatNewPasswd);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblNewPasswd);
+            this.Controls.Add(this.txtCurrentPasswd);
+            this.Controls.Add(this.btnChangePassword);
+            this.Controls.Add(this.lblCurrentPasswd);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "EditUserFrom";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "EditUserFrom";
+            this.Load += new System.EventHandler(this.EditUserFrom_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.TextBox txtPhoneNumber;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.TextBox txtCurrentPasswd;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblCurrentPasswd;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnChangePassword;
+        private System.Windows.Forms.Label lblNewPasswd;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Label lblCi;
+        private System.Windows.Forms.Label lblRepeatNewPasswd;
+        private System.Windows.Forms.TextBox txtNewPasswd;
+        private System.Windows.Forms.TextBox txtRepeatNewPasswd;
+        private System.Windows.Forms.Button btnUpdateUser;
     }
 }
--- Presentation/Forms/EditUserFrom.cs
+++ Presentation/Forms/EditUserFrom.cs
@@ -1,20 +1,103 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Obligatorio.Domain.Model;
+using Obligatorio.Services.Interfaces;
 
 namespace Presentation.Forms
 {
     public partial class EditUserFrom : Form
     {
-        public EditUserFrom()
+        private readonly IUserService _userService;
+        public EditUserFrom(IUserService userService)
         {
+            _userService = userService;
             InitializeComponent();
+        }
+
+        private void btnChangePassword_Click(object sender, EventArgs e)
+        {
+            if (!lblCurrentPasswd.Visible)
+            {
+                btnChangePassword.Text = "Cancelar";
+            }
+            else
+            {
+                btnChangePassword.Text = "Cambiar contraseña";
+            }
+
+            // change lables state
+            lblCurrentPasswd.Visible = !lblCurrentPasswd.Visible;
+            lblNewPasswd.Visible = !lblNewPasswd.Visible;
+            lblRepeatNewPasswd.Visible = !lblRepeatNewPasswd.Visible;
+
+            // change textboxes state
+            txtCurrentPasswd.Visible = !txtCurrentPasswd.Visible;
+            txtNewPasswd.Visible = !txtNewPasswd.Visible;
+            txtRepeatNewPasswd.Visible = !txtRepeatNewPasswd.Visible;
+        }
+
+        private void btnUpdateUser_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Usuario updatedUser = new Usuario()
+                {
+                    Cedula = Global.LoggedUser.Cedula,
+                    NombreUsuario = Global.LoggedUser.NombreUsuario,
+                    Nombre = txtName.Text,
+                    Apellido = txtSurname.Text,
+                    Correo = txtEmail.Text,
+                    Telefono = int.Parse(txtPhoneNumber.Text)
+                };
+
+                if (txtCurrentPasswd.Visible)
+                {
+                    if(_userService.IsUserAllowedToChangePassword(Global.LoggedUser.NombreUsuario, txtCurrentPasswd.Text))
+                    {
+                        if (txtNewPasswd.Text.Equals(txtRepeatNewPasswd.Text))
+                        {
+                            updatedUser.Contrasenia = txtNewPasswd.Text;
+                        }
+                        else
+                        {
+                            MessageBox.Show("La nueva contraseña debe coincidir en los dos campos solicitados");
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Contraseña anterior incorrecta");
+                        return;
+                    }
+                }
+
+                _userService.Modify(updatedUser);
+
+                MessageBox.Show("Datos actualizados correctamente");
+
+                txtCurrentPasswd.Text = "";
+                txtNewPasswd.Text = "";
+                txtRepeatNewPasswd.Text = "";
+            }
+            catch(InvalidOperationException io)
+            {
+                MessageBox.Show(io.Message);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private void EditUserFrom_Load(object sender, EventArgs e)
+        {
+            lblCi.Text = lblCi.Text.Replace("[Ci]", Global.LoggedUser.Cedula.ToString());
+            lblUsername.Text = lblUsername.Text.Replace("[Username]", Global.LoggedUser.NombreUsuario.ToString());
+
+            txtName.Text = Global.LoggedUser.Nombre.ToString();
+            txtSurname.Text = Global.LoggedUser.Apellido.ToString();
+            txtEmail.Text = Global.LoggedUser.Correo.ToString();
+            txtPhoneNumber.Text = Global.LoggedUser.Telefono.ToString();
         }
     }
 }
